68d4c4ef8a87447de85b791ea3d8096f7dcd95c2
May Alqunaytir <mayalqunaytir@gmail.com>
2025-12-24 14:39:33 +0300

    Initial Commit

---
+++ Movieisme/MovieismeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieismeApp.swift
+//  Movieisme
+//
+//  Created by May Alqunaytir on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieismeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
